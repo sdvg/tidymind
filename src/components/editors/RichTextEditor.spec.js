27c5e09f5f8bdbdfe4b1072af7d401140ff3c93c
Stefan Dietz <sdvg@users.noreply.github.com>
2018-07-15 15:39:25 +0200

    Add tests for RichTextEditor

---
+++ src/components/editors/RichTextEditor.spec.js
@@ -0,0 +1,36 @@
+/* eslint-env jest */
+
+import tinymce from 'tinymce';
+import RichTextEditor from './RichTextEditor'
+import { shallowMount } from '@vue/test-utils'
+
+jest.mock(`tinymce/themes/modern/theme`, () => {})
+jest.mock(`tinymce/skins/lightgray/skin.min.css`, () => {})
+jest.mock(`tinymce/skins/lightgray/content.min.css`, () => {})
+jest.mock(`tinymce`, () => ({
+  init: jest.fn(),
+  remove: jest.fn(),
+}))
+
+describe(`RichTextEditor`, () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(RichTextEditor, {
+      propsData: {
+        document: {
+          content: ``,
+        },
+      },
+    })
+  })
+
+  it(`initializes tinyMCE on mount`, () => {
+    expect(tinymce.init).toHaveBeenCalled()
+  })
+
+  it(`removes tinyMCE on destroy`, () => {
+    wrapper.destroy()
+    expect(tinymce.remove).toHaveBeenCalled()
+  })
+})
